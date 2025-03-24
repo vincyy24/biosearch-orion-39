d8f56c688871d29f2564616cf49b6046bd38fd19
vincyy24 <vaibhav.k.codes@gmail.com>
2025-03-24 17:53:47 +0530

    Refactor: Update .gitignore and add vite.py template tag

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 1000, // Increase warning limit to 1000kb
+    manifest: true,
     rollupOptions: {
       output: {
         manualChunks: {
